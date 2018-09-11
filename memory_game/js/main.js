e80201f3feffdc639eb3767e32616d3ed270d333
Kristen-K <mac@macs-mbp.home>
2018-09-11 13:48:16 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,22 +2,33 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+
+};
 
 
-var checkLength = function(){
+var flipcard = function(cardId){
+ var checkLength = function(){
   if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
   } else {
   	alert("Sorry, try again");
   }
 }
+checkForMatch();
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 
 checkLength();
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+};
+
+flipcard(0)
+flipcard(2)
