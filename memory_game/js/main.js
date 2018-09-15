0b4fbc8566c6f7bff00cdf41fbea5205f429e121
Kristen-K <kkerchhoff88@gmail.com>
2018-09-15 12:07:41 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,5 @@
 
+// variable array 
 var cards = [
 {
 	rank: "queen",
@@ -25,41 +26,67 @@ var cards = [
 
 var cardsInPlay = [];
 
+//function - checks for match and displays alert
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match");
-        //score +=1;
+       
     } else {
         alert("Sorry, try again.");
+        
     }
 };
 
+//function for flipcard
 var flipCard = function() {
+	//assigns user's clicks to variable
 	var cardId = this.getAttribute('data-id');
+
 	// randomises cards array
+	// 0.5 - Math.random() - returns random numbers roughly 50% negative and 50% positive.
+
 	cards.sort(function(a, b){return 0.5 - Math.random()});
+
+	// pushes user's .rank selection into cardsinplay
     cardsInPlay.push(cards[cardId].rank);
+
+    // displays image and suit
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
+
+    // displays card image
     this.setAttribute('src', cards[cardId].cardImage);
 
+	// checks if 2 cards have been selected. If yes, checks for match
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
-    console.log("User flipped " + cards[cardId].rank);
     
 };
 
+
 var createBoard = function() {
+	
+	//for loop for cards array
 	for (var i = 0; i < cards.length; i++) {
+
+		// creates image element 
 		var cardElement = document.createElement('img');
+
+		//sets attibutes for cardElement 
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
+
+		// adds event listener - when clicked, runs flipcard method call
 		cardElement.addEventListener('click', flipCard);
+
+		// appends cards to board using ID
 		document.getElementById("game-board").appendChild(cardElement);
 	}
+
 };
 
+// method call to start game 
 createBoard();
 
 
